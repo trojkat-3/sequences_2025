c0af1bc6e779a08f3a3a74e29fd52acba4edeb4b
trojkat <michal.stukow@ug.edu.pl>
2025-10-06 11:19:38 +0200

    Completed getNumberOfDigits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
 public class Main {
+
+    static final int MAX = 2000;
+    static final int MAX_IN_LINE = 20;
+
+    static int getNumberOfDigits(int n) {
+        int nd = 0;
+        while (n > 0) {
+            nd++;
+            n = (n - n % 10) / 10;
+        }
+        return nd;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int maxNoDigits = getNumberOfDigits(MAX);
+        for (int i = 1; i <= MAX; i++) {
+            for (int j = 0; j < maxNoDigits - getNumberOfDigits(i); j++) {
+                System.out.print(" ");
+            }
+            System.out.print(i);
+            if (i < MAX) {
+                System.out.print(", ");
+            }
+            if (i % MAX_IN_LINE == 0) {
+                System.out.print("\n");
+            }
+        }
     }
 }
